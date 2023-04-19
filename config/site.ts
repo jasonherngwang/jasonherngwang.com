fbbcb095888f3c0552eb5a80d8d15f4662d77099
Jason Wang <jasonherngwang@gmail.com>
2023-04-19 08:45:58 -0700

    chore: update config, linting, packages

--- config/site.ts
+++ config/site.ts
@@ -5,4 +5,5 @@ export const siteConfig: SiteConfig = {
   description:
     'Jason Wang is a full-stack software engineer with experience in JavaScript, Ruby, React, Tailwind, Express, PostgreSQL, Docker, and AWS.',
   url: 'https://jasonherngwang.com',
+  ogImage: 'https://jasonherngwang.com/og.png',
 };
